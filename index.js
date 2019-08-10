d27759b34b93bef03f1d57b17c3cb5be33cd8975
carlosmendoza <30907555+carlosmendoza@users.noreply.github.com>
2019-08-10 17:56:19 -0500

    Update index.js
    Effect of name

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
-'use strict'
+// Animación para la entrada del nombre
+var Animator = {
+  animate: function(options){
 
-// Funciones anonimas
-// Es una función que no tiene nombre
-var formulario= document.querySelector("#formulario");
-formulario.addEventListener("submit", function(){
-	var nombre = document.querySelector("#nombre");
-console.log(nombre);
-})
+    options.stagger = options.stagger || 200;
+    options.staggerPerLetter = options.staggerPerLetter || 50;
+
+    let el = document.querySelector(options.selector);
+
+    let text = el.innerText;
+    el.innerText = "";
+
+    for(letter_index in text){
+
+      let letter = text[letter_index];
+      console.log(letter);
+      let span = document.createElement('span');
+      span.innerText = ' ';
+      el.appendChild(span);
+
+
+      setTimeout(()=>{
+        this.animateLetter({
+          element: span,
+          letter: letter,
+          stagger: options.staggerPerLetter
+        })
+      },options.stagger*letter_index)
+
+    }
+  },
+  animateLetter: function(options,contador = 0){
+    if(contador > 20) return options.element.innerText = options.letter;
+
+    contador++;
+
+    setTimeout(()=>{
+      options.element.innerText = this.generateRandomChar();
+      this.animateLetter(options,contador);
+    },options.stagger)
+  },
+  generateRandomChar: function(){
+    return Math.random().toString(36).substr(2,1);
+  }
+}
+//
+//
