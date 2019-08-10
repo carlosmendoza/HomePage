34094d5895fa08e76597c3da724d31ebe0fdf3e4
Carlos Mendoza <ca.mendoza@uniandes.edu.co>
2019-08-10 07:01:33 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// Funciones anonimas
+// Es una función que no tiene nombre
+var formulario= document.querySelector("#formulario");
+formulario.addEventListener("submit", function(){
+	var nombre = document.querySelector("#nombre");
+console.log(nombre);
+})
