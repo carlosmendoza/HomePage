509eda5ff5421a0f4c91e3eef81870054c86f090
carlosmendoza <ca.mendoza@uniandes.edu.co>
2019-08-17 22:40:56 -0500

    scroll

--- index.js
+++ index.js
@@ -88,4 +88,20 @@ function cargaTodos()
   carga("card3");
 }
 
+$(document).ready(function() {
+  $('a[href^="#"]').click(function() {
+    var destino = $(this.hash);
+    if (destino.length == 0) {
+      destino = $('a[name="' + this.hash.substr(1) + '"]');
+    }
+    if (destino.length == 0) {
+      destino = $('html');
+    }
+    $('html, body').animate({ scrollTop: destino.offset().top }, 500);
+    return false;
+  });
+});
+
+
+
 
